e0ac2b6ee12811e644231160e05366ef85e8a8e8
Daniel Tamayo <elkin.zapata@est.iudigital.edu.co>
2024-09-17 21:57:45 -0500

    Se crean clases con sus atributos y métodos.

--- src/IUDigital/Departamento.java
+++ src/IUDigital/Departamento.java
@@ -7,6 +7,7 @@ public class Departamento {
     private String id;
     private String nombre;
     private List<Empleado> empleados;
+    private static final int MAX_EMPLEADOS = 5; // Límite de empleados por departamento
 
     public Departamento(String id, String nombre) {
         this.id = id;
@@ -26,9 +27,14 @@ public class Departamento {
         return empleados;
     }
 
-    public void agregarEmpleado(Empleado empleado) {
-        empleados.add(empleado);
-        empleado.setDepartamentoId(this.id);
+    public boolean agregarEmpleado(Empleado empleado) {
+        if (empleados.size() < MAX_EMPLEADOS) {
+            empleados.add(empleado);
+            empleado.setDepartamentoId(this.id);
+            return true;  // Se agregó el empleado con éxito
+        } else {
+            return false; // No se puede agregar más empleados
+        }
     }
 
     public void eliminarEmpleado(Empleado empleado) {
